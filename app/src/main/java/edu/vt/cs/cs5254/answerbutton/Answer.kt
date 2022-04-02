0a4fe37bd55067c7b07f4570a6ab34fcfdef1678
Pei Cheng Huang <peicheng@PeideMacBook-Pro.local>
2022-04-01 23:28:22 -0400

    initial commit

---
+++ app/src/main/java/edu/vt/cs/cs5254/answerbutton/Answer.kt
@@ -0,0 +1,6 @@
+import androidx.annotation.StringRes
+
+data class Answer (@StringRes val textResId: Int,
+                   val isCorrect: Boolean,
+                   var isEnabled: Boolean = true,
+                   var isSelected: Boolean = false)
